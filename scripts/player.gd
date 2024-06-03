9d2376645e6d8111dddf5e3aaf7f1b4c250dd061
Abrar Alolyan <abrar-ol@users.noreply.github.com>
2024-06-03 17:51:16 +0300

    apply force to the bullet for shooting

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @export var speed = 400
+var bullet_speed = 2000
+var bullet = preload("res://scenes/bullet.tscn")
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -8,5 +10,15 @@ func get_input():
 	look_at(get_global_mouse_position())
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		fire()
 	get_input()
 	move_and_slide()
+
+func fire():
+	var bullet_instance =  bullet.instantiate()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(bullet_speed, 0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child",bullet_instance)
+
